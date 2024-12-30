ad4f905df9fc52c392b9977e74a25046e8fc2331
Margarida Grilo <margaridagrilo7@gmail.com>
2024-12-30 15:20:43 +0000

    docs: read me update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import Fastify from 'fastify';
+// routes
+import productRoutes from './routes/products';
 
 const server = Fastify({
 	logger: true,
 });
 
 // register routes
-server.register(require('./routes/products'));
+server.register(productRoutes);
 
 server.get('/', () => {
 	return 'Hello from PenPal Market API!';
